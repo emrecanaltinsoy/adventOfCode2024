b1450ef1042d1c04b5de85a65737500459c55f50
Emrecan Altinsoy <emrecan.altinsoy@se.com>
2024-12-05 13:27:59 +0100

    Day2Challenge1 completed

---
+++ day2/main.go
@@ -0,0 +1,100 @@
+package day2
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func Day1Challenge1() {
+	fmt.Println("Day1Challenge1")
+
+	file, err := os.Open("./day2/challenge1.txt")
+	if err != nil {
+		fmt.Println("could not read file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		locationIds, err := StringsToIntegers(strings.Split(line, " "))
+		if err != nil {
+			fmt.Println("Error converting to integer")
+		}
+		var doesIncrease bool
+		var isValid bool
+		for index := range locationIds[:len(locationIds)-1] {
+			difference := locationIds[index+1] - locationIds[index]
+			if index == 0 {
+				if difference == 0 || difference > 3 || difference < -3 {
+					isValid = false
+					break
+				}
+				if difference > 0 {
+					doesIncrease = true
+				} else {
+					doesIncrease = false
+				}
+			} else {
+				if difference == 0 || difference > 3 || difference < -3 {
+					isValid = false
+					break
+				}
+				if difference > 0 && !doesIncrease {
+					isValid = false
+					break
+				} else if difference < 0 && doesIncrease {
+					isValid = false
+					break
+				} else {
+					isValid = true
+				}
+			}
+		}
+		if isValid {
+			total++
+		}
+	}
+	fmt.Println("total", total)
+}
+
+func Day1Challenge2() {
+	fmt.Println("Day1Challenge2")
+
+	file, err := os.Open("./day1/challenge2.txt")
+	if err != nil {
+		fmt.Println("could not read file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var firstList []int
+	locationIDMap := make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		locationIds, err := StringsToIntegers(strings.Split(line, "   "))
+		if err != nil {
+			fmt.Println("Error converting to integer")
+		}
+		if !slices.Contains(firstList, locationIds[0]) {
+			firstList = append(firstList, locationIds[0])
+		}
+
+		val, ok := locationIDMap[locationIds[1]]
+		if ok {
+			locationIDMap[locationIds[1]] = val + 1
+		} else {
+			locationIDMap[locationIds[1]] = 1
+		}
+	}
+	total := 0
+	for _, firstListItem := range firstList {
+		total += firstListItem * locationIDMap[firstListItem]
+	}
+
+	fmt.Println(total)
+}
--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"github.com/emrecanaltinsoy/adventOfCode2024/day1"
+	// "github.com/emrecanaltinsoy/adventOfCode2024/day1"
+	"github.com/emrecanaltinsoy/adventOfCode2024/day2"
 )
 
 func main() {
-	day1.Day1Challenge1()
-	day1.Day1Challenge2()
+	// day1.Day1Challenge1()
+	// day1.Day1Challenge2()
+	day2.Day1Challenge1()
+	// day2.Day1Challenge2()
 }
