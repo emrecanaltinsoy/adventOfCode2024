6c45f6f87b551652d067ec6e5b369b4c19e70f09
Emrecan Altinsoy <emrecanaltinsoy@gmail.com>
2024-12-07 19:40:02 +0100

    Day2Challenge1 rewrite

--- day2/main.go
+++ day2/main.go
@@ -4,9 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"slices"
 	"strings"
 )
+
+func IsBadLevel(difference int) bool {
+	return difference == 0 || difference > 3 || difference < -3
+}
+
+func All[T any](ts []T, pred func(T) bool) bool {
+	for _, t := range ts {
+		if !pred(t) {
+			return false
+		}
+	}
+	return true
+}
 
 func Day2Challenge1() {
 	fmt.Println("Day2Challenge1")
@@ -17,6 +29,9 @@ func Day2Challenge1() {
 	}
 	defer file.Close()
 
+	IsPositive := func(value int) bool { return value > 0 }
+	IsNegative := func(value int) bool { return value < 0 }
+
 	scanner := bufio.NewScanner(file)
 	total := 0
 	for scanner.Scan() {
@@ -25,34 +40,20 @@ func Day2Challenge1() {
 		if err != nil {
 			fmt.Println("Error converting to integer")
 		}
-		var doesIncrease bool
-		var isValid bool
+		isValid := true
+		var differences []int
 		for index := range locationIds[:len(locationIds)-1] {
-			difference := locationIds[index+1] - locationIds[index]
-			if index == 0 {
-				if difference == 0 || difference > 3 || difference < -3 {
-					isValid = false
-					break
-				}
-				if difference > 0 {
-					doesIncrease = true
-				} else {
-					doesIncrease = false
-				}
-			} else {
-				if difference == 0 || difference > 3 || difference < -3 {
-					isValid = false
-					break
-				}
-				if difference > 0 && !doesIncrease {
-					isValid = false
-					break
-				} else if difference < 0 && doesIncrease {
-					isValid = false
-					break
-				} else {
-					isValid = true
-				}
+			differences = append(differences, locationIds[index+1]-locationIds[index])
+		}
+		if !All(differences, IsPositive) && !All(differences, IsNegative) {
+			isValid = false
+			continue
+		}
+
+		for _, diff := range differences {
+			if IsBadLevel(diff) {
+				isValid = false
+				break
 			}
 		}
 		if isValid {
@@ -65,36 +66,42 @@ func Day2Challenge1() {
 func Day2Challenge2() {
 	fmt.Println("Day2Challenge2")
 
-	file, err := os.Open("./day1/challenge2.txt")
+	file, err := os.Open("./day2/challenge2.txt")
 	if err != nil {
 		fmt.Println("could not read file")
 	}
 	defer file.Close()
 
+	IsPositive := func(value int) bool { return value > 0 }
+	IsNegative := func(value int) bool { return value < 0 }
+
 	scanner := bufio.NewScanner(file)
-	var firstList []int
-	locationIDMap := make(map[int]int)
+	total := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		locationIds, err := StringsToIntegers(strings.Split(line, "   "))
+		locationIds, err := StringsToIntegers(strings.Split(line, " "))
 		if err != nil {
 			fmt.Println("Error converting to integer")
 		}
-		if !slices.Contains(firstList, locationIds[0]) {
-			firstList = append(firstList, locationIds[0])
+		isValid := true
+		var differences []int
+		for index := range locationIds[:len(locationIds)-1] {
+			differences = append(differences, locationIds[index+1]-locationIds[index])
+		}
+		if !All(differences, IsPositive) && !All(differences, IsNegative) {
+			isValid = false
+			continue
 		}
 
-		val, ok := locationIDMap[locationIds[1]]
-		if ok {
-			locationIDMap[locationIds[1]] = val + 1
-		} else {
-			locationIDMap[locationIds[1]] = 1
+		for _, diff := range differences {
+			if IsBadLevel(diff) {
+				isValid = false
+				break
+			}
+		}
+		if isValid {
+			total++
 		}
 	}
-	total := 0
-	for _, firstListItem := range firstList {
-		total += firstListItem * locationIDMap[firstListItem]
-	}
-
-	fmt.Println(total)
+	fmt.Println("total", total)
 }
